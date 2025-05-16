b63cb1b6142705fd8f996c3a6b67b278d7c440ac
lelletocka <linnea.valtersson@hotmail.se>
2025-05-16 18:05:26 +0200

    finish assignment

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 
-let random = Math.floor(Math.random() * 10) + 1;
-let random2 = Math.floor(Math.random() * 10) + 1;
-let random3 = Math.floor(Math.random() * 10) + 1;
+//prompt and alert strings in Swedish because of using my kids as test subjects
 
-console.log(random);
-console.log(random2);
-console.log(random3);
+const random = Math.floor(Math.random() * 10) + 1;
+let guess;
+let correctAnswer;
+
+
+while (!correctAnswer) {
+    guess = parseFloat(prompt("Gissa ett nummer mellan 1-10!"));
+    if (guess === random) {
+        correctAnswer = true;
+        console.log("Du gissade rätt! Woho!");
+    } else if (guess < random) {
+        alert("Inte helt rätt! Prova ett högre nummer!");
+    } else {
+        alert("Inte helt rätt! Prova ett lägre nummer!");
+    }
+}
