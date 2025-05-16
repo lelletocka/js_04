2987b8ecf9109982c4aa923a44ba4b5547445d4b
lelletocka <linnea.valtersson@hotmail.se>
2025-05-16 11:57:04 +0200

    test code snippet

---
+++ main.js
@@ -0,0 +1,8 @@
+
+let random = Math.floor(Math.random() * 10) + 1;
+let random2 = Math.floor(Math.random() * 10) + 1;
+let random3 = Math.floor(Math.random() * 10) + 1;
+
+console.log(random);
+console.log(random2);
+console.log(random3);
